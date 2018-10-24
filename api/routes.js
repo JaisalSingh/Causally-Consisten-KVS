321e8c9e90ff0671b9f3f66342dce67a3aeb6c3b
Jimmy Nguyen <JimmyNguyen@jimmys-mbp-3.attlocal.net>
2018-10-24 01:32:55 -0700

    started working on first search and delete

--- api/routes.js
+++ api/routes.js
@@ -24,8 +24,8 @@ module.exports = function (app, db) {
 		// 		'result': 'Error',
 		// 		'msg': 'Object too large. Size limit is 1MB'
 		// 	});
-		// else 
-		
+		// else
+
 		if(req.params.key.length < 1 || req.params.key.length > 200)
 			res.json({
 				'result': 'Error',
@@ -36,9 +36,9 @@ module.exports = function (app, db) {
 			if(keyValueStore.hasKey(req.params.key)) {
 				res.status(200);
 				responseBody.msg = "Updated successfully";
-				if(keyValueStore.set(req.params.key, req.body.val)) 
+				if(keyValueStore.set(req.params.key, req.body.val))
 					responseBody.replaced = "True";
-				else 
+				else
 					responseBody.replaced = "False";
 			} else {
 				keyValueStore.set(req.params.key, req.body.val);
@@ -59,6 +59,10 @@ module.exports = function (app, db) {
 		res.send('POST message received: ' + req.query.msg);
 	});
 
+	app.post("/hello", function(req, res) {
+		res.status(405).end();
+	});
+
 	app.get('/test', (req, res) => {
 		res.send('GET request received');
 	});
@@ -67,7 +71,55 @@ module.exports = function (app, db) {
 		res.send("Hello world!");
 	});
 
-	app.post("/hello", function(req, res) {
-		res.status(405).end();
+	app.searchKey('/search', (req, res) => {
+		if(keyValueStore.hasKey(req.param.key)){
+			res.status(200);
+			/*
+			res.json({
+				'result': 'Success',
+				'isExists': 'true'
+			});
+			*/
+			responseBody.result = "Success"
+			responseBody.isExists = "true"
+		}else{
+			res.status(201);
+			/*
+			res.json({
+				'result': 'Error',
+				'isExists': 'false'
+			});
+			*/
+			responseBody.result = "Failed"
+			responseBody.isExists = "false"
+		}
+		res.json(responseBody);
 	});
+
+	app.returnKey('/search', (req, res) => {
+
+	});
+
+	app.delete('/keyValue-store/:key', (req, res) => {
+		if(keyValueStore.hasKey(req.param.key)){
+			res.status(200);
+			keyValueStore.remove(req.params.key, req.body.val);
+			/*
+			res.json({
+				'result': 'Success'
+			});
+			*/
+			responseBody.result = "Success"
+		}else{
+			res.status(404);
+			/* res.json({
+				'result': 'Error',
+				'msg': 'Status code 404'
+			}); */
+			responseBody.result = "Failed"
+			responseBody.msg = "Status code 404"
+	}
+	res.json(responseBody);
+);
+
 }
