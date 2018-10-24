d0802f0ce636360642ea82942fdf01c97b4bceb8
Jimmy Nguyen <JimmyNguyen@jimmys-mbp-3.attlocal.net>
2018-10-24 02:18:03 -0700

    working on value size limitation

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 var express = require('express'),
   	app = express(),
 	port = process.env.PORT || 8080,
-	bodyParser = require('body-parser'); 
+	bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: true }));//, limit:"1MB" }));
-app.use(bodyParser.json());
+// Original code
+// app.use(bodyParser.urlencoded({ extended: true }));//, limit:"1MB" }));
+// app.use(bodyParser.json());
+
+app.use(bodyParser.json({ limit: '1mb' }));
+app.use(bodyParser.urlenencoded({ limit: '1mb', extended: true}));
 
 require('./api/') (app, {});
 
