75d6bbd9fdaa9c05b4211af0054f9c76f2929866
Jaisal Singh <jsingh@pulsesecure.net>
2018-11-17 14:14:21 -0800

    made it so it removes the old images before building a new one

--- rebuilddocker.bat
+++ rebuilddocker.bat
@@ -3,9 +3,10 @@ setlocal enabledelayedexpansion
 SET image_name=grslin/gjjr_kvs
 SET ip=172.20.99.161
 
-docker build -t %image_name% .
-
 docker rm -f replica1 replica2 replica3
+docker rmi %image_name%
+
+docker build -t %image_name% .
 
 docker run -d --name=replica1 -p 8080:8080 -e VIEW="%ip%:8080,%ip%:8081,%ip%:8082" -e IP_PORT="%ip%:8080" %image_name%
 docker run -d --name=replica2 -p 8081:8080 -e VIEW="%ip%:8080,%ip%:8081,%ip%:8082" -e IP_PORT="%ip%:8080" %image_name%
